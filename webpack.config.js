7d7a6e84aabefc747bffbd8499d8acced9b461e5
Silas Owu <owusilasyp00@gmail.com>
2024-09-10 03:52:59 -0700

    feat: implement task 3, use webpack as the main build tool

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+
+    
+    devtool: 'source-map',
+
+    // Task #7: Setup a local development server using webpack-dev-server
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 5000,
+        hot: true,
+    },
+    mode: 'development',
+
+    // Task#9: Optimize the production build
+    optimization: {  
+        splitChunks: { 
+            chunks: 'all',  
+        },  
+        minimize: true,
+    },
+};
